0ff91a315dc722927e18863fe28ec85154877c20
rithyc <48160519+rithyc@users.noreply.github.com>
2022-03-10 18:02:44 -0500

    Update vmssnew_u20.sh

--- scripts/vmssnew_u20.sh
+++ scripts/vmssnew_u20.sh
@@ -1,52 +1,37 @@
 #!/bin/bash
 
-DEBIAN_FRONTEND=noninteractive apt-get -y update
+timeStamp=$(date +'%y%m%d_%H%M%S')
+echo "=> VMSS Extension Started [$timeStamp]"
 
-
-printf "\n\n\n[`date +'%Y/%m/%d %H:%M:%S'`] Installing packages...\n\n\n"
-
-# safer ones to avoid interrupting the live prod system esp during YPP...
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install mysql-client git graphviz aspell
-
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install php7.4 php7.4-bcmath php7.4-bz2 php7.4-cli php7.4-common php7.4-curl php7.4-dev php7.4-fpm php7.4-gd php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-pgsql php7.4-readline php7.4-soap php7.4-xml php7.4-xmlrpc php7.4-zip php7.4-igbinary php7.4-redis php-pear
-sleep 1s
-sudo service php7.4-fpm stop
-
-sudo apt-get -y install nginx
-sleep 1s
-sudo service nginx stop
-
-# no longer use varnish
-
-printf "\n\n\n[`date +'%Y/%m/%d %H:%M:%S'`] Copying configuration baseline...\n\n\n"
-
-cd /
-tar zxf /moodle/scripts/nodebaseline/srv_certs.tar.gz
-
-cd /
-rm -rf /mnt/localcache /mnt/.opcache
-mkdir -p /mnt/localcache
-mkdir -p /mnt/.opcache
-chown -R 33:33 /mnt/localcache /mnt/.opcache
-
-if [ -d "/var/www/html/moodle" ]; then
-  mv /var/www/html/moodle /var/www/html/moodle_orig
+# Specific for this instance
+if [[ $1 =~ ^gluster-vm-(.+)$ ]] && [[ $2 =~ ^controller-vm-(.+)$ ]]; then
+  glusterNode="$1"
+  controllerNode="$2"
+else
+  printf "Syntax: $0 <glusterVMName> <controllerVMName>\n"
+  exit 10
 fi
-cd /
-tar zxf /moodle/scripts/nodebaseline/var_www_html_moodle_azure_patched.tar.gz
+glusterVolume=data
 
-cd /
-rm -rf /etc/nginx /etc/php
-tar zxf /moodle/scripts/nodebaseline/etc_nginx_php_ulimit_ssl_azure.tar.gz
+set -x
 
-cd /
-tar zxf /moodle/scripts/nodebaseline/lib_systemd_system_services_azure.tar.gz
-systemctl daemon-reload
-sleep 3s
+# gluster
+#sudo add-apt-repository ppa:gluster/glusterfs-3.10 -y
+sudo apt-get -y update
+sudo apt-get -y install glusterfs-client
 
-printf "\n\n\n[`date +'%Y/%m/%d %H:%M:%S'`] Starting services...\n\n\n"
+# Mount gluster fs for /moodle
+sudo mkdir -p /moodle
+sudo chown www-data /moodle
+sudo chmod 770 /moodle
+sudo echo -e 'Adding Gluster FS to /etc/fstab and mounting it'
+grep -q "/moodle.*glusterfs" /etc/fstab || echo "$glusterNode:/$glusterVolume /moodle glusterfs defaults,_netdev,log-level=WARNING,log-file=/var/log/gluster.log 0 0" >> /etc/fstab
+mount /moodle
 
-sudo service php7.4-fpm start
-sudo service nginx start
+# look for the chained configurator
+echo "=> Chaining the custom node configuration"
+if [ -f /moodle/scripts/ubuntu20_setupnewnode.sh ]; then
+  /bin/bash /moodle/scripts/ubuntu20_setupnewnode.sh "$controllerNode" > /var/log/ubuntu20_setupnewnode.log
+fi
 
-printf "\n\n\n[`date +'%Y/%m/%d %H:%M:%S'`] *** New Node Done...\n\n\n"
+echo "=> VMSS Extension Done [$timeStamp]"
